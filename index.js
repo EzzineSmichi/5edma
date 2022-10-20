609998e70a61cd6894bef94e88dad3b382a0d980
EzzineSmichi <ezzinesmichi2@gmail.com>
2022-10-20 22:58:38 +0100

    connect to data base

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
-const port = 5000;
+require("dotenv").config();
+
+mongoose
+  .connect(`${process.env.DATA_BASE_URL}`)
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Error...", err);
+  });
+
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`server run on port ${port}`));
